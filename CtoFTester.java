bd2d3475828c2e1ec5ebeb45d6faba994177eba5
Gabriel Small <76072280+64br13l-5@users.noreply.github.com>
2021-09-23 17:11:19 -0400

    Update CtoFTester.java

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,3 +1,13 @@
 public class CtoFTester{
-  //this is a test lol
+    public static void main(String[]args){
+        System.out.println(celsiustoFahrenheit(25));
+        System.out.println(fahrenheitToCelsius(57));
+
+    }
+    public static int celsiustoFahrenheit(int c){
+        return c*(9/5)+32;
+    }
+    public static int fahrenheitToCelsius(int f){
+        return (f-32)/(9/5);
+    }
 }
