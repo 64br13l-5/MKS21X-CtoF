9970399093d2d778acd76d7a9f5efdf2ecb82a15
Gabriel Small <76072280+64br13l-5@users.noreply.github.com>
2021-09-23 22:25:43 -0400

    Update CtoFTester.java

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,3 +1,4 @@
+// The functions are integers, that take in the parameter of celsius or fahrenheight. It returns a converted integer of the opposite temperature, which will also be ints.
 public class CtoFTester{
     public static void main(String[]args){
         System.out.println(celsiustoFahrenheit(25));
